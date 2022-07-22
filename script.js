79f9d35ca9aca302567c7315fbccb105dabbdb16
Ashutosh0602 <85889617+Ashutosh0602@users.noreply.github.com>
2022-07-23 00:43:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const video = document.getElementById("videoid");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let isVideos = false;
+let isGame = true;
+const car_pos = document
+  .getElementsByClassName("object_car")[0]
+  .getBoundingClientRect();
 
 let model = null;
+let total_score = 0;
+
 const modelParams = {
   flipHorizontal: true,
   outputStride: 16,
@@ -24,6 +30,7 @@ function startVideo() {
       console.log("video is recording");
       isVideos = true;
       runDetection();
+      call_generateObstacles();
     }
   });
 }
@@ -38,10 +45,8 @@ function runDetection() {
         document
           .getElementsByClassName("object")[0]
           .classList.add("object_pos_left");
-        console.log("left le liya");
       }
       if (pred[0].bbox[0] > 250) {
-        console.log("right le liya");
         document
           .getElementsByClassName("object")[0]
           .classList.remove("object_pos_left");
@@ -49,9 +54,7 @@ function runDetection() {
           .getElementsByClassName("object")[0]
           .classList.add("object_pos_right");
       }
-    } catch (err) {
-      console.log("shi nhi chal rha");
-    }
+    } catch (err) {}
     model.renderPredictions(predictions, canvas, context, video);
     if (isVideos) {
       requestAnimationFrame(runDetection);
@@ -64,44 +67,100 @@ handTrack.load(modelParams).then((lmodel) => {
   console.log(lmodel);
 });
 
-function toggle() {
-  console.log("bta toggle karu ya na karu");
-  isVideos = true;
+function game_head() {
   if (isVideos) {
-    startVideo();
+    document.getElementsByClassName("score_start")[0].style.display = "none";
+    document.getElementsByClassName("object")[0].style.display = "block";
+  }
+  if (!isVideos) {
+    document.getElementsByClassName("score_start")[0].style.display = "flex";
+    document.getElementsByClassName("score_result")[0].innerText = total_score;
+    total_score = 0;
+    document.getElementsByClassName("object")[0].style.display = "none";
   }
 }
 
 function generateObstacles() {
-  let obs_div = document.createElement("div");
-  let push_div = document.getElementsByClassName("road")[0];
-  let side = ["right", "left"];
-  let side_no = side[Math.floor(Math.random() * 2)];
-  console.log(side_no);
-  push_div.appendChild(obs_div);
+  const obs_pos_ls = ["right", "left"];
+  const obs_pos = obs_pos_ls[Math.floor(Math.random() * 2)];
+  const obs_div = document.createElement("div");
+  const obs_img = document.createElement("IMG");
+  obs_img.setAttribute("src", "./rock.png");
+  obs_div.appendChild(obs_img);
   obs_div.classList.add("object_obstacles");
-  obs_div.innerText = "asdhoe";
-  if (side_no == "right") {
-    obs_div.style.right = "0";
-  } else {
-    obs_div.style.left = "0";
-  }
-  obs_div.style.top = "0%";
 
-  let obs_pos = 0;
-  function moveObstacles() {
-    obs_div.style.top = `${obs_pos}%`;
-    obs_pos += 0.1;
+  const obs_cont = document.getElementsByClassName("road")[0];
+  obs_cont.appendChild(obs_div);
+
+  obs_div.style.top = "0%";
+  if (obs_pos == "right") {
+    obs_div.style.right = "0";
+  } else obs_div.style.left = "0";
+
+  const obs_cur_pos = obs_div.getBoundingClientRect();
+  let obs_pos_top = 0;
+  function moveobstacles() {
+    const car_cur_pos = document
+      .getElementsByClassName("object")[0]
+      .getBoundingClientRect();
+
+    if (
+      obs_cur_pos.x === car_cur_pos.x &&
+      obs_div.getBoundingClientRect().y - 15 <= car_cur_pos.y &&
+      car_cur_pos.y <= obs_div.getBoundingClientRect().y + 15
+    ) {
+      handTrack.stopVideo(video);
+      isVideos = false;
+      game_head();
+      clearInterval(call);
+      console.log(Math.floor(total_score / 10));
+      console.log("mil gya");
+    }
+    if (obs_div.getBoundingClientRect().y > car_cur_pos.y) {
+      total_score++;
+    }
+
+    const rem_pos = obs_div.style.top;
+
+    if (rem_pos.split("%")[0] > 110) {
+      let div_rem = obs_div;
+      div_rem.remove();
+    }
+
+    obs_div.style.top = `${obs_pos_top}%`;
+    obs_pos_top += 0.5;
   }
-  let gen_obs = setInterval(() => {
-    generateObstacles();
-    return clearInterval(gen_obs);
-  }, 5000);
-  let move_obs = setInterval(() => {
-    moveObstacles();
-    return () => {
-      clearInterval(move_obs);
-    };
-  }, 1);
+  const move = setInterval(() => {
+    moveobstacles();
+    return () => clearInterval(move);
+  }, 10);
 }
-// generateObstacles();
+function call_generateObstacles() {
+  const call = setInterval(() => {
+    if (isVideos) {
+      generateObstacles();
+      return () => clearInterval(call);
+    } else {
+      clearInterval(call);
+    }
+  }, 3000);
+  return call;
+}
+
+function toggle() {
+  isVideos = true;
+  if (isVideos) {
+    startVideo();
+    isVideos = true;
+    game_head();
+  }
+}
+
+console.log(
+  "obstacle",
+  document.getElementsByClassName("object_obstacles")[0].getBoundingClientRect()
+);
+console.log(
+  "car",
+  document.getElementsByClassName("object")[0].getBoundingClientRect()
+);
